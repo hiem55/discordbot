992a219c02a2f6ed2ec7e88a2fb8cb995c4309b1
Justin Oh <oh.justin55@gmail.com>
2022-12-25 18:47:08 -0800

    test

--- discordbot.py
+++ discordbot.py
@@ -1,39 +1,27 @@
 import re
 import os
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 bot = commands.Bot(command_prefix='$')
 TOKEN = os.getenv('DISCORD_TOKEN')
+
 
 @bot.event
 async def on_ready():
     print("We are logged in as {0.user}!".format(bot))
 
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    
+
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    await bot.process_commands(message)
 
 
 bot.run(TOKEN)
